1cc6a41ae2f0fe02119e4324e8ea62b272934fa5
Giovane Limas Salvi <giovanelimas16@hotmail.com>
2022-07-20 18:19:37 -0300

    button drawing

--- src/Calculator/ElementGraphic/Button.java
+++ src/Calculator/ElementGraphic/Button.java
@@ -4,93 +4,56 @@ import Calculator.Manager.GraphicManager;
 import Calculator.Math.Math;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Button {
-    Math size = new Math(50,50);
-    public Button(){
-        newButton();
+    private Math size;
+    private Math position;
+    private String content;
+    private JButton jButton;
+    private boolean isNumber;
+    private boolean isSpecial;
+    private static JFrame window = GraphicManager.getWindow();
+
+    public Button(String content, Math position, boolean isNumber, boolean isSpecial){
+        this.content = content;
+        this.isNumber = isNumber;
+        this.isSpecial = isSpecial;
+        size = new Math(50, 50);
+        this.position = position;
+        jButton = new JButton(content);
+        if(jButton == null){
+            System.out.println("ERROR: jButton is null");
+            System.exit(0);
+        }
+        startup();
     }
-    private void newButton(){
-        JFrame window = GraphicManager.getWindow();
 
-        JButton b1 = new JButton("/");
-        b1.setBounds( 160, 150 + size.y * 0, size.x, size.y);
-        window.add(b1);
+    private void startup(){
+        //window.setLayout(null);
+        jButton.setBackground(Color.BLACK);
+        if(isNumber){
+            jButton.setForeground(Color.WHITE);
+        } else if(!isSpecial) {
+            jButton.setForeground(Color.GREEN);
+        } else {
+            jButton.setForeground(Color.ORANGE);
+        }
+        jButton.setBounds(position.x, position.y, size.x, size.y);
+        window.add(jButton);
+    }
 
-        JButton b2 = new JButton("x");
-        b2.setBounds( 160, 150 + size.y * 1, size.x, size.y);
-        window.add(b2);
+    public Math getSize(){
+        return size;
+    }
 
-        JButton b3 = new JButton("-");
-        b3.setBounds(160, 150 + size.y * 2, size.x, size.y);
-        window.add(b3);
+    public void setSize(Math size){
+        this.size = size;
+    }
 
-        JButton b4 = new JButton("+");
-        b4.setBounds(160, 150 + size.y * 3, size.x, size.y);
-        window.add(b4);
-
-        JButton b5 = new JButton("=");
-        b5.setBounds(160, 150 + size.y * 4, size.x, size.y);
-        window.add(b5);
-
-        JButton b6 = new JButton("%");
-        b6.setBounds(160 - size.x * 1, 150 + size.y * 0, size.x, size.y);
-        window.add(b6);
-
-        JButton b7 = new JButton("()");
-        b7.setBounds(160 - size.x * 2, 150 + size.y * 0, size.x, size.y);
-        window.add(b7);
-
-        JButton b8 = new JButton("C");
-        b8.setBounds(160 - size.x * 3, 150 + size.y * 0, size.x, size.y);
-        window.add(b8);
-
-        JButton b9 = new JButton("7");
-        b9.setBounds(160 - size.x * 3, 150 + size.y * 1, size.x, size.y);
-        window.add(b9);
-
-        JButton b10 = new JButton("8");
-        b10.setBounds(160 - size.x * 2, 150 + size.y * 1, size.x, size.y);
-        window.add(b10);
-
-        JButton b11 = new JButton("9");
-        b11.setBounds(160 - size.x * 1, 150 + size.y * 1, size.x, size.y);
-        window.add(b11);
-
-        JButton b12 = new JButton("4");
-        b12.setBounds(160 - size.x * 3, 150 + size.y * 2, size.x, size.y);
-        window.add(b12);
-
-        JButton b13 = new JButton("5");
-        b13.setBounds(160 - size.x * 2, 150 + size.y * 2, size.x, size.y);
-        window.add(b13);
-
-        JButton b14 = new JButton("6");
-        b14.setBounds(160 - size.x * 1, 150 + size.y * 2, size.x, size.y);
-        window.add(b14);
-
-        JButton b15 = new JButton("1");
-        b15.setBounds(160 - size.x * 3, 150 + size.y * 3, size.x, size.y);
-        window.add(b15);
-
-        JButton b16 = new JButton("2");
-        b16.setBounds(160 - size.x * 2, 150 + size.y * 3, size.x, size.y);
-        window.add(b16);
-
-        JButton b17 = new JButton("3");
-        b17.setBounds(160 - size.x * 1, 150 + size.y * 3, size.x, size.y);
-        window.add(b17);
-
-        JButton b18 = new JButton("+/-");
-        b18.setBounds(160 - size.x * 3, 150 + size.y * 4, size.x, size.y);
-        window.add(b18);
-
-        JButton b19 = new JButton("0");
-        b19.setBounds(160 - size.x * 2, 150 + size.y * 4, size.x, size.y);
-        window.add(b19);
-
-        JButton b20 = new JButton(",");
-        b20.setBounds(160 - size.x * 1, 150 + size.y * 4, size.x, size.y);
-        window.add(b20);
+    public JButton getButton(){
+        return jButton;
     }
 }
--- src/Calculator/Manager/GraphicManager.java
+++ src/Calculator/Manager/GraphicManager.java
@@ -3,21 +3,103 @@ package Calculator.Manager;
 import Calculator.ElementGraphic.Button;
 import Calculator.Math.Math;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class GraphicManager {
-    private static JFrame window = new JFrame();
-    private Math math = new Math(235, 450);
+    private static JFrame window;
+    private Math display;
+    private static List<Button> buttons;
+
     public GraphicManager(){
+        window = new JFrame();
+        if(window == null){
+            System.out.println("ERROR: window is null");
+            System.exit(0);
+        }
+        buttons = new ArrayList<>();
+        if(buttons == null){
+            System.out.println("ERROR: buttons is null");
+            System.exit(0);
+        }
+        display = new Math(235, 450);
         newWindow();
-        new Button();
+        newButtons();
     }
     public void newWindow(){
         window.setTitle("Calculator");
-        window.setSize(math.x, math.y);
+        window.setSize(display.x, display.y);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+    }
+
+    public void newButtons(){
+
+        short space = 0;
+
+        Button b1 = new Button("/", new Math(160 + space * 4, 150 + space), false, false);
+        buttons.add(b1);
+
+        Button b2 = new Button("x", new Math(160 + space * 4, 150 + b1.getSize().y * 1 + space), false, false);
+        buttons.add(b2);
+
+        Button b3 = new Button("-", new Math(160 + space * 4, 150 + b1.getSize().y * 2 + space), false, false);
+        buttons.add(b3);
+
+        Button b4 = new Button("+", new Math(160 + space * 4, 150 + b1.getSize().y * 3 + space), false, false);
+        buttons.add(b4);
+
+        Button b5 = new Button("=", new Math(160 + space * 4, 150 + b1.getSize().y * 4 + space), false, true);
+        buttons.add(b5);
+
+
+        Button b6 = new Button("%", new Math(160 - b1.getSize().x * 1 + space * 3, 150 + b1.getSize().y * 0 + space), false, false);
+        buttons.add(b6);
+
+        Button b7 = new Button("()", new Math(160 - b1.getSize().x * 2 + space * 2, 150 + b1.getSize().y * 0 + space), false, false);
+        buttons.add(b7);
+
+        Button b8 = new Button("C", new Math(160 - b1.getSize().x * 3 + space * 1, 150 + b1.getSize().y * 0 + space), false, true);
+        buttons.add(b8);
+
+        Button b9 = new Button("7", new Math(160 - b1.getSize().x * 3 + space * 1, 150 + b1.getSize().y * 1 + space), true, false);
+        buttons.add(b9);
+
+        Button b10 = new Button("8", new Math(160 - b1.getSize().x * 2 + space * 2, 150 + b1.getSize().y * 1 + space), true, false);
+        buttons.add(b10);
+
+        Button b11 = new Button("9", new Math(160 - b1.getSize().x * 1 + space * 3, 150 + b1.getSize().y * 1 + space), true, false);
+        buttons.add(b11);
+
+        Button b12 = new Button("4", new Math(160 - b1.getSize().x * 3 + space * 1, 150 + b1.getSize().y * 2 + space), true, false);
+        buttons.add(b12);
+
+        Button b13 = new Button("5", new Math(160 - b1.getSize().x * 2 + space * 2, 150 + b1.getSize().y * 2 + space), true, false);
+        buttons.add(b13);
+
+        Button b14 = new Button("6", new Math(160 - b1.getSize().x * 1 + space * 3, 150 + b1.getSize().y * 2 + space), true, false);
+        buttons.add(b14);
+
+        Button b15 = new Button("1", new Math(160 - b1.getSize().x * 3 + space * 1, 150 + b1.getSize().y * 3 + space), true, false);
+        buttons.add(b15);
+
+        Button b16 = new Button("2", new Math(160 - b1.getSize().x * 2 + space * 2, 150 + b1.getSize().y * 3 + space), true, false);
+        buttons.add(b16);
+
+        Button b17 = new Button("3", new Math(160 - b1.getSize().x * 1 + space * 3, 150 + b1.getSize().y * 3 + space), true, false);
+        buttons.add(b17);
+
+        Button b18 = new Button("+/-", new Math(160 - b1.getSize().x * 3 + space * 1, 150 + b1.getSize().y * 4 + space), false, false);
+        buttons.add(b18);
+
+        Button b19 = new Button("0", new Math(160 - b1.getSize().x * 2 + space * 2, 150 + b1.getSize().y * 4 + space), true, false);
+        buttons.add(b19);
+
+        Button b20 = new Button(",", new Math(160 - b1.getSize().x * 1 + space * 3, 150 + b1.getSize().y * 4 + space), false, false);
+        buttons.add(b20);
     }
 
     public static JFrame getWindow(){
